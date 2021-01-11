c71984a7165d49f321a7a5703ef3145677e1cc01
chenzhuo <332006721@qq.com>
2021-01-11 17:28:30 +0800

    add 12 roman ;

---
+++ exam/13/index.ts
@@ -0,0 +1,34 @@
+type ROMAN = "I" | "V" | "X" | "L" | "C" | "D" | "M";
+
+const romanNumber: { [key in ROMAN]: number } = {
+  I: 1,
+  V: 5,
+  X: 10,
+  L: 50,
+  C: 100,
+  D: 500,
+  M: 1000,
+};
+
+function romanToInt(s: string): number {
+  let total = 0;
+
+  const romanList = s.split("") as ROMAN[];
+
+  let prechar = romanNumber[romanList[0]];
+  for (let i = 1; i < romanList.length; i++) {
+    const char = romanNumber[romanList[i]] ?? 0;
+    if (prechar < char) {
+      total -= prechar;
+    } else {
+      total += prechar;
+    }
+    prechar = char;
+  }
+
+  total += prechar;
+
+  return total;
+}
+
+console.log(romanToInt("MCMXCIV"));
